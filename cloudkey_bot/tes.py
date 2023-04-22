0cb0f8046f338bc7849ba03fd97769a54fe3e25b
Zhauner <mojo4everhd@gmail.com>
2023-04-22 15:36:16 +0300

    check gitignore

--- cloudkey_bot/tes.py
+++
@@ -1,6 +0,0 @@
-
-from django.conf import settings
-from django.contrib.auth.hashers import check_password
-
-settings.configure()
-print(check_password('000000000', 'pbkdf2_sha256$600000$Uxm52MmLGPDIsqAtCG7jnK$qr9U/50+q/FrOFS8byKWKRaYvvqMSJ+dvdzBC+d67DQ='))
